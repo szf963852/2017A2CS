2949a59c1bc21e2f902b64652848dfdec363f78a
szf963852 <34550762+szf963852@users.noreply.github.com>
2018-03-26 16:02:01 +0800

    hw
    hw

---
+++ ch29/chapter29.pl
@@ -0,0 +1,15 @@
+capitalCity(paris).
+capitalCity(berlin).
+capitalCity(cairo).
+
+father(F, C) :- 
+	male(F), parent(F, C).
+
+ancestor(A, B) :- 
+	parent(A, B).
+
+ancestor(A, B) :- 
+	parent(A, X), ancestor(X, B).
+
+writeList ([]).
+writeList([HiTJ) :-write (H), nl, writeList (T). 
